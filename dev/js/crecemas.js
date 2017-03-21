d7c50980fa64a72cef88a304f9e9587010fde419
David Chay <davichay@gmail.com>
2017-03-20 21:24:21 -0600

    terminado la seccion de contacto

--- dev/js/crecemas.js
+++ dev/js/crecemas.js
@@ -34,3 +34,36 @@
     });  
     
 })(jQuery);
+
+
+
+
+
+var map;
+var contentMap="<h5>Cree Ce Más, S.A. de C.V.</h5>";
+contentMap+="<p>23 calle oriente no. 91, colonia Centro, Tapachula, Chiapas.</p>";
+contentMap+="<p>TEL: (962) 1180560; CEL: 962 695 6131</p>";
+$(document).ready(function(){
+  //prettyPrint();
+  map = new GMaps({
+    div: '#map',
+    lat: 14.915300,
+    lng: -92.251479,
+    scrollwheel: false,
+    scroll:{x:$(window).scrollLeft(),y:$(window).scrollTop()},
+    zoom: 16,
+  });
+ 
+  map.addMarker({
+    lat: 14.915300,
+    lng: -92.251479,
+    title: 'Marker with InfoWindow',
+    infoWindow: {
+      content: contentMap
+    }
+  });
+});
+  
+
+//AIzaSyDsvPx6JrySQxAm9Q94aWXmAMXugaRXyMY
+//AIzaSyDsvPx6JrySQxAm9Q94aWXmAMXugaRXyMY
